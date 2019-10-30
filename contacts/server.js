870d3f519988a1e684663217e54a649d9202d0e9
divya-500 <divya.lingabathula@500apps.com>
2019-10-30 22:45:31 +0530

    Add Api calls

--- contacts/server.js
+++ contacts/server.js
@@ -3,9 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use((bodyParser.urlencoded({extended: true})))
+app.use(bodyParser.json())
 
 var connection = mysql.createConnection({
     host:'173.194.107.65',
@@ -17,6 +18,47 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if(err) throw err
     console.log('now u r connected with db ....')
+})
+
+app.get('/contacts', function(req, res){
+    connection.query('select * from contact', function(err, ressults, fields){
+        if(err) throw err;
+        res.end(JSON.stringify(ressults))
+
+    })
+})
+
+app.get('/contacts/:id', function(req, res){
+    connection.query('select * from contact where id = ?', [req.params.id],function(err, ressults, fields){
+        if(err) throw err;
+        res.end(JSON.stringify(ressults))
+
+    })
+})
+
+app.post('/contacts', function(req, res){
+    var params = req.body;
+    console.log(params)
+    connection.query('insert into contact set ?', params, function(err, ressults, fields){
+        if(err) throw err;
+        //console.log(fields)
+        res.end(JSON.stringify(ressults.insertId));
+
+    })
+})
+ 
+app.put('/contacts', function(req, res){
+    connection.query('update contact set first_name=?, last_name=? where id=? ',[req.body.first_name,req.body.last_name, req.body.id], function(err, result, fileds){
+        if(err) throw err;
+        res.end(JSON.stringify(result))
+    })
+})
+
+app.delete('/contacts', function(req, res){
+    connection.query('delete from contact where id = ?', [req.body.id], function(err, result, fields){
+        if(err) throw err;
+        res.end(`${req.body.id} has been deleted ${result}`)
+    })
 })
 
 app.get('/', function(req, res){
